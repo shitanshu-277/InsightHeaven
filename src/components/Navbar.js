af562edfe272ca97ef4af1b2f98a794768cd8fd5
shitanshu-277 <shitanshugoyal2002@gmail.com>
2024-01-14 15:16:01 +0530

    change title of news app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = ()=>{
       <div>
         <nav className="navbar fixed-top navbar-expand-lg bg-body-teritary bg-dark" data-bs-theme="dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Navbar</Link>
+    <Link className="navbar-brand" to="/">InsightHaven</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
